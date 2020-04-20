fc22deb659674bc4875786879273dcec28c8bf7c
[muneneee] <[lukekevin39@gmail.com]>
2020-04-20 16:38:25 +0300

    created a credentials class

--- passsafe.py
+++ passsafe.py
@@ -35,4 +35,34 @@ class User:
         return cls.user_list
 
 
-    
+class Credentials:
+    user_list = []
+
+
+    def __init__(self,name,username,account,password):
+
+        self.name = name
+        self.username = username
+        self.account = account
+        self.password = password
+
+    def save_user(self):
+        '''
+        a method for saving the user
+        '''
+        User.user_list.append(self)
+
+
+    def delete_user(self):
+        '''
+        method for deleting users
+        '''
+        User.user_list.remove(self)
+
+
+    @classmethod
+    def display_details(cls):
+        '''
+        method to display user details
+        '''
+        return cls.user_list
